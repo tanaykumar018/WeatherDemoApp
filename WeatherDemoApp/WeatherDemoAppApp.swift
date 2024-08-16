75e3e8d560a94b6327a4adbb7342501f2a5e2c2b
tanaykumar018 <tanaykumar018@gmail.com>
2024-08-16 09:43:12 -0500

    Initial Commit

---
+++ WeatherDemoApp/WeatherDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoAppApp.swift
+//  WeatherDemoApp
+//
+//  Created by Tanay Kumar Roy on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
